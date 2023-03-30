239931f5ffdf7fcde33ee2497ea24b52cdc8d034
cr-velox-git <khangame78@gmail.com>
2023-03-30 12:55:50 +0530

    permission check, request and internetConnectivityListener added

--- lazeir/src/main/java/com/velox/lazeir/MainActivity.kt
+++ lazeir/src/main/java/com/velox/lazeir/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.velox.lazeir
 
-import android.Manifest
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.velox.lazeir.utils.checkForPermission
-import com.velox.lazeir.utils.internetConnectivityListener
 import dagger.hilt.android.AndroidEntryPoint
 
 
--- lazeir/src/main/java/com/velox/lazeir/utils/Converter.kt
+++ lazeir/src/main/java/com/velox/lazeir/utils/Converter.kt
@@ -25,7 +25,7 @@ fun Uri.toByteArray(context: Context): ByteArray? {
     val byteBuffer = ByteArrayOutputStream()
     val bufferSize = 1024
     val buffer = ByteArray(bufferSize)
-    var len = 0
+    var len: Int
     return if (iStream != null) {
         while (iStream.read(buffer).also { len = it } != -1) {
             byteBuffer.write(buffer, 0, len)
