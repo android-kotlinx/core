da4936024fab1ded5af01b2ad0502c770abcf0ff
khanrajesh <rajesh.k@iserveu.co.in>
2023-07-15 15:41:14 +0530

    added doc

--- lazeir/src/main/java/com/velox/lazeir/utils/outlet/InternetConnetivityObserverOulet.kt
+++ lazeir/src/main/java/com/velox/lazeir/utils/outlet/InternetConnetivityObserverOulet.kt
@@ -5,6 +5,26 @@ import androidx.lifecycle.LifecycleCoroutineScope
 import com.velox.lazeir.utils.internetObserver
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+
+/**
+ * [internetConnectivityListener] is a observer which keep observing connectivity of the network
+ * and it attaches it self to the lifecycleScope, and uses ConnectivityManager in background
+ *
+ * Example
+ *
+ * import androidx.lifecycle.lifecycleScope
+ *
+ *
+internetConnectivityListener(lifecycleScope = lifecycleScope,
+context = context,
+onAvailable = {},
+onLosing = {},
+onLost = {},
+onUnAvailable = {})
+
+ *
+ * it returns  "Available, Unavailable, Losing, Lost" state on connectivity change in the above functions respectively
+ * **/
 @OptIn(ExperimentalCoroutinesApi::class)
 fun internetConnectivityListener(
     lifecycleScope: LifecycleCoroutineScope,
