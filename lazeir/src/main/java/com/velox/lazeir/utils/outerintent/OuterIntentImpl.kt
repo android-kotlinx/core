41522f9d3efebd8654cdc8d08e59bde4f89b94a1
khanrajesh <rajesh.k@iserveu.co.in>
2023-07-15 14:25:52 +0530

    added outer intent function and change in intent specific data type transfer

--- lazeir/src/main/java/com/velox/lazeir/utils/outerintent/OuterIntentImpl.kt
+++ lazeir/src/main/java/com/velox/lazeir/utils/outerintent/OuterIntentImpl.kt
@@ -3,9 +3,12 @@ package com.velox.lazeir.utils.outerintent
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
 import android.widget.Toast
+import java.io.Serializable
 
-class OuterIntentImpl:OuterIntentInterface {
+class OuterIntentImpl : OuterIntentInterface {
     override fun <T> startActivity(
         context: Context,
         packageName: String,
@@ -16,18 +19,38 @@ class OuterIntentImpl:OuterIntentInterface {
             val action = "$packageName.$activityName"
             val intent = Intent(action)
             intent.setPackage(packageName)
-            if (dataToIntent != null){
+            if (dataToIntent != null) {
                 for (data in dataToIntent) {
-                    val (key,value) = data
-                    when(value){
-                        is String -> intent.putExtra(key,value as String)
-                        is Int -> intent.putExtra(key,value as Int)
+                    val (key, value) = data
+                    when (value) {
+                        is String -> intent.putExtra(key, value as String)
+                        is Int -> intent.putExtra(key, value as Int)
+                        is Boolean -> intent.putExtra(key, value as Boolean)
+                        is Bundle -> intent.putExtra(key, value as Bundle)
+                        is CharSequence -> intent.putExtra(key, value as CharSequence)
+                        is BooleanArray -> intent.putExtra(key, value as BooleanArray)
+                        is Byte -> intent.putExtra(key, value as Byte)
+                        is ByteArray -> intent.putExtra(key, value as ByteArray)
+                        is Char -> intent.putExtra(key, value as Char)
+                        is CharArray -> intent.putExtra(key, value as CharArray)
+                        is Double -> intent.putExtra(key, value as Double)
+                        is DoubleArray -> intent.putExtra(key, value as DoubleArray)
+                        is Float -> intent.putExtra(key, value as Float)
+                        is FloatArray -> intent.putExtra(key, value as FloatArray)
+                        is IntArray -> intent.putExtra(key, value as IntArray)
+                        is Long -> intent.putExtra(key, value as Long)
+                        is LongArray -> intent.putExtra(key, value as LongArray)
+                        is Short -> intent.putExtra(key, value as Short)
+                        is ShortArray -> intent.putExtra(key, value as ShortArray)
+                        is Parcelable -> intent.putExtra(key, value as Parcelable)
+                        is Serializable -> intent.putExtra(key, value as Serializable)
                     }
                 }
             }
-            context.startActivity(intent)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(context, "Unable to start $activityName", Toast.LENGTH_SHORT).show()
-        }
+        context.startActivity(intent)
+    } catch (e: ActivityNotFoundException)
+    {
+        Toast.makeText(context, "Unable to start $activityName", Toast.LENGTH_SHORT).show()
     }
+}
 }
